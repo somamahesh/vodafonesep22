3bb435dc60ec35d9644dabcf10bea273c213f6e1
somamahesh <37290341+somamahesh@users.noreply.github.com>
2022-09-23 13:20:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-vodafonesep22
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_instance" "myawsserver" {
+  ami = var.image
+  instance_type = "t2.nano"
+ # key_name = "gagan-new"
+  tags = {
+    Name = lower (var.name)
+    env = upper (var.env)
+    owner = upper ("Techlanders")
+    dept = "Eng"
+    client = "vodafone-1"
+  }
+}
+
